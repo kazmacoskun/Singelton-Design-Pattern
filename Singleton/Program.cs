81314481a769a95c8fc980398de8871ee4c99e15
Coskun Kazma <coskun.kazma@gmail.com>
2024-04-11 02:07:27 +0300

    Singleton Design Pattern
    Singleton Design Pattern

---
+++ Singleton/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var  customeManager = CustomerManager.CreateAsSingleton();           
+            customeManager.Save();
+        }
+    }
+    class CustomerManager
+    {
+        private static CustomerManager _customerManager;
+        static object _lockObject = new object();
+        public CustomerManager()
+        {
+
+        }
+        public static CustomerManager CreateAsSingleton()
+        {
+            lock (_lockObject)
+            {
+                if (_customerManager == null)
+                {
+                    _customerManager = new CustomerManager();
+                }
+                return _customerManager;
+            }
+        }
+        public void Save()
+        {
+            Console.WriteLine("Saved!!");
+            Console.Read();
+        }
+    }
+}
